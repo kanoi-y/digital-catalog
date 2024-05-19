91889e6094ac49b6d0fd9ecc33ad6d6b31d222bc
kanoi-y <furafuratonbo@gmail.com>
2024-05-20 00:26:24 +0900

    ✨ 画像の描画設定を実装

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import "./style.css";
 
 const app = document.getElementById("app") as HTMLDivElement;
+const loading = document.getElementById("loading") as HTMLDivElement;
 
 const canvas = document.createElement("canvas");
 canvas.id = "canvas";
 
 app.appendChild(canvas);
 
-const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
 if (!ctx) {
-  throw new Error('Failed to get 2d context from canvas');
+  throw new Error("Failed to get 2d context from canvas");
 }
 
 console.log(ctx);
@@ -18,12 +19,26 @@ console.log(ctx);
 const loadImg = (img: HTMLImageElement): Promise<HTMLImageElement> => {
   return new Promise((resolve, reject) => {
     img.onload = () => resolve(img);
-    img.onerror = () => reject(new Error('Failed to load image'));
+    img.onerror = () => reject(new Error("Failed to load image"));
   });
 };
 
+let aspectRatio = 0;
+let imgHeight = 0;
+let imgWidth = 0;
+
+const setting = (img: HTMLImageElement) => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  aspectRatio = img.width / img.height;
+  imgHeight = canvas.height;
+  imgWidth = imgHeight * aspectRatio;
+};
+
 const image = new Image();
-image.src = '/vite.svg';
-loadImg(image).then(() => {
-  ctx.drawImage(image, 15, 15);
+image.src = "/vite.svg";
+loadImg(image).then(async (img) => {
+  loading.remove();
+  setting(img);
+  ctx.drawImage(image, 0, 0, imgWidth, imgHeight);
 });
